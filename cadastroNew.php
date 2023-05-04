374217f7c34702d3396aeed0c92c5ae1daccc710
Richard Aydan Fiuza Ost <122907674+aydanfiuza@users.noreply.github.com>
2023-05-03 21:35:44 -0300

    algumas alterações

--- cadastroNew.php
+++ cadastroNew.php
@@ -13,6 +13,7 @@
 
     <link rel="stylesheet" href="<?= $BASE_URL ?>/css/cadastroNew.css">
     <link rel="stylesheet" href="<?= $BASE_URL ?>/css/responsive.css">
+    <link rel="stylesheet" href="<?= $BASE_URL ?>/css/style.css">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 
--- templates/nav.php
+++ templates/nav.php
@@ -31,23 +31,31 @@
       <!-- FIM IF -->
       <?php endif; ?>
         
-      <li id="home" class="<?= $link['li'] ?>">
+      <a href="<?= $BASE_URL ?>/index.php"><li id="home" class="<?= $link['li'] ?>">
         <i <?php if (isset($username)) {echo 'id="cor0"';}?> class="<?= $link['i'] ?> fa-solid fa-house"></i>
         <span>Home</span>
-      </li>
-      <li id="entrada" class="<?= $link['li'] ?>">
+      </li></a>
+
+      <a href="<?= $BASE_URL ?>/cadastroNew.php"><li id="cadastro" class="<?= $link['li'] ?>">
+        <i <?php if (isset($username)) {echo 'id="cor1"';}?> class="<?= $link['i'] ?> fa-solid fa-arrow-up-from-bracket"></i>
+        <span>Cadastro</span>
+      </li></a>
+
+      <a href="<?= $BASE_URL ?>/entrada.php"><li id="entrada" class="<?= $link['li'] ?>">
         <i <?php if (isset($username)) {echo 'id="cor2"';}?> class="<?= $link['li'] ?> fa-solid fa-plus"></i>
         <span>Entrada</span>
-      </li>
+      </li></a>
+
       <li id="saida" class="<?= $link['li'] ?>">
         <i <?php if (isset($username)) {echo 'id="cor3"';}?> class="<?= $link['li'] ?> fa-solid fa-minus"></i>
         <span>Saída</span>
       </li>
+
       <li id="relatorio" class="<?= $link['li'] ?>">
         <i <?php if (isset($username)) {echo 'id="cor4"';}?> class="<?= $link['li'] ?> fa-solid fa-file"></i>
         <span>Relatório</span>
       </li>
+
     </ul>
   </nav>
 </header>
-
